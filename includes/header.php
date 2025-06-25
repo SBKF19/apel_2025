d5dd5c61487f1f2d0a69cc42a3d6ba384e085843
SBKF19 <sofiane.benkherouf05@gmail.com>
2025-06-25 18:50:50 +0200

    Gestion complète de l'inscription d'une carte + les erreurs

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,11 @@
 <?php session_start();
 include_once 'includes/database.php';
+$currentlink = $_SERVER['REQUEST_URI'];
+// Check if the user is logged in, if not redirect to login page
+if (!isset($_SESSION['utilisateur']) && (!str_contains($currentlink, 'connexion.php'))) {
+    header('Location: connexion.php');
+    exit();
+}
 ?>
 <!DOCTYPE html>
 <html lang="fr">
--- inscription.php
+++ inscription.php
@@ -1,17 +1,28 @@
 <?php
 include_once 'includes/header.php';
-if ($_SESSION['utilisateur']['role'] === 'eleve' && $_SESSION['utilisateur']['email'] !== null) {
+if ($_SESSION['utilisateur']['role'] === 'eleve' && $_SESSION['utilisateur']['email'] !== null ) {
     header('Location: recap.php');
     exit();
 }
-if ($_SESSION['utilisateur'])
+if ($_SESSION['utilisateur']) {
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $card = trim($_POST['card']);
         $mail = trim($_POST['mail']);
         $birth = trim($_POST['birth']);
+        $consent = isset($_POST['consent']) ? 1 : 0;
+        $errorCard = '';
+        $errorVide = '';
+        $errorNai = '';
+        $errorCase = '';
 
-        if (!empty($card) && !empty($mail) && !empty($birth)) {
+        if (empty($card) || empty($mail) || empty($birth)) {
+            $errorVide = "Veuillez remplir tous les champs.";
+        } elseif ($birth != $_SESSION['utilisateur']['date_nai']) {
+            $errorNai = "La date de naissance ne correspond pas à celle de votre compte.";
+        } elseif ($consent == 0) {
+            $errorCase = "Veuillez cocher la case pour donner votre consentement.";
+        } elseif (!empty($card) && !empty($mail) && !empty($birth) && $consent == 1 && $birth == $_SESSION['utilisateur']['date_nai']) {
             $requete1 = $connexion->prepare(
                 'UPDATE utilisateur
             SET email_uti = :mail,
@@ -37,9 +48,11 @@ if ($_SESSION['utilisateur'])
 
             header('Location: recap.php');
             exit();
+
+
         }
     }
-
+}
 ?>
 <header>
 
@@ -54,24 +67,36 @@ if ($_SESSION['utilisateur'])
         <p class="midtitle">Veuillez renseigner votre numéro de carte, e-mail et date de naissance</p>
         <form action="" method="post">
             <div class="column">
+                <?php if (isset($errorVide)): ?>
+                    <p class="error"><?= htmlspecialchars($errorVide) ?></p>
+                <?php endif; ?>
+                <?php if (isset($errorCard) && ($errorCard != "")): ?>
+                    <p class="error"><?= htmlspecialchars($errorCard) ?></p>
+                <?php endif; ?>
                 <label for="name">N° de carte</label>
                 <input class="input" type="text" id="card" name="card" required>
                 <label class="spaceTop" for="name">adresse mail</label>
                 <input class="input" type="mail" id="mail" name="mail" required>
+                <?php if (isset($errorNai) && ($errorNai != "")): ?>
+                    <p class="error"><?= htmlspecialchars($errorNai) ?></p>
+                <?php endif; ?>
                 <label class="spaceTop" for="name">Date de naissance</label>
                 <input class="input" type="date" id="birth" name="birth" required>
                 <button class="button buttonConnect" type="submit">Se connecter</button>
             </div>
+            <?php if (isset($errorCase) && ($errorCase != "")): ?>
+                <p class="error"><?= htmlspecialchars($errorCase) ?></p>
+            <?php endif; ?>
+            <label class="row fit">
+                <input class="checkbox" type="checkbox" id="consent" name="consent" required>
+                <p class="conditions">Je donne l’autorisation à l’Apel du Lycée Saint-Vincent pour prélever sur ma carte
+                    «Génération HDF» la somme de : <b>55€</b> pour les élèves de seconde, <b>65€</b> pour les élèves de
+                    première,
+                    <b>75€</b> pour
+                    les élèves de terminales.
+                </p>
+            </label>
         </form>
-        <label class="row fit">
-            <input class="checkbox" type="checkbox" required>
-            <p class="conditions">Je donne l’autorisation à l’Apel du Lycée Saint-Vincent pour prélever sur ma carte
-                «Génération HDF» la somme de : <b>55€</b> pour les élèves de seconde, <b>65€</b> pour les élèves de
-                première,
-                <b>75€</b> pour
-                les élèves de terminales.
-            </p>
-        </label>
 
         <p>Un problème ? <a href="mail.php" class="blueText noDeco">Cliquez ici</a> pour nous envoyer un
             e-mail
